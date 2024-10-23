9e98442ea14be4eb9eec4a0328d149e82482d55a
trustacean <pizzacrustacean15@gmail.com>
2024-10-23 22:39:01 +0700

    chore: added barebone route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import Navbar from "./components/core/Navbar"
+import { AnimatePresence } from "framer-motion";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/core/Navbar";
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
       <Navbar />
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+        </Routes>
+      </AnimatePresence>
     </>
-  )
+  );
 }
 
 export default App
