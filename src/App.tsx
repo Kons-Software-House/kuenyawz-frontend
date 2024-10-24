2e3e035c4e6cd386bf9d02d9910867bbeed87d69
trustacean <pizzacrustacean15@gmail.com>
2024-10-24 19:58:09 +0700

    feat: update App.tsx added path to about us view

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { AnimatePresence } from "framer-motion";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Navbar from "./components/core/Navbar";
+import AboutUsView from "./views/AboutUsView";
 
 function App() {
   const location = useLocation();
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
+          <Route path="/tentang-kami" element={<AboutUsView />} />
         </Routes>
       </AnimatePresence>
     </>
