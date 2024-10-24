22b0c5e0212d00416985a8b222066a1df3dd7abc
trustacean <pizzacrustacean15@gmail.com>
2024-10-24 19:58:23 +0700

    feat: create Colors.tsx

---
+++ src/components/core/Colors.tsx
@@ -0,0 +1,13 @@
+export type BackgroundColors = {
+    background?: 'bg-secondary-200' | 'bg-tetriary-100' | 'bg-tetriary-200' | 'bg-tetriary-300' | 'bg-tetriary-400' | 'bg-tetriary-500' | 'bg-tetriary-600';
+}
+
+export const LighterBackgroundColors = {
+    'bg-secondary-200': 'bg-secondary-250',
+    'bg-tetriary-100': 'bg-tetriary-150',
+    'bg-tetriary-200': 'bg-tetriary-250',
+    'bg-tetriary-300': 'bg-tetriary-350',
+    'bg-tetriary-400': 'bg-tetriary-450',
+    'bg-tetriary-500': 'bg-tetriary-550',
+    'bg-tetriary-600': 'bg-tetriary-650',
+}
