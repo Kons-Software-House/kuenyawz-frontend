ae4b4e30d27bca518ce8561e713f981aa680872f
trustacean <pizzacrustacean15@gmail.com>
2024-10-25 19:07:30 +0700

    feat: added function to scroll page to top when location changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { useEffect } from "react";
 import { AnimatePresence } from "framer-motion";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Navbar from "./components/core/Navbar";
 import LandingView from "./views/LandingView";
+import AboutUsView from "./views/AboutUsView";
 
 function App() {
   const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
 
   return (
     <>
@@ -12,10 +18,11 @@ function App() {
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
           <Route path="/" element={<LandingView />} />
+          <Route path="/tentang-kami" element={<AboutUsView />} />
         </Routes>
       </AnimatePresence>
     </>
   );
 }
 
-export default App
+export default App;
