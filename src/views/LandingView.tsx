d48efa119a2f74a74ee4df1f8a0859dff96584ca
trustacean <pizzacrustacean15@gmail.com>
2024-10-26 15:29:43 +0700

    chore: changed a element to Link

--- src/views/LandingView.tsx
+++ src/views/LandingView.tsx
@@ -1,21 +1,24 @@
+import { Link } from "react-router-dom";
 import HorizontalScroller from "../components/LandingView/HorizontalScroller";
+import TransitionLayer from "../components/core/TransitionLayer";
 
 export default function LandingView() {
   return (
     <>
+      <TransitionLayer background='bg-secondary-200' />
       <div className='h-16 bg-secondary-100'>
       </div>
       <HorizontalScroller />
       <div className='h-[28rem] grid grid-cols-3'>
-        <Column background='bg-secondary-300' header='Kontak Kami' description='Ciptakan Pertemuan Manis' href='/contact'>
+        <Column background='bg-secondary-300' header='Produk Kami' description='Kelezatan Dalam Setiap Kreasi' href='/menu'>
           <div className='bg-white h-1/2 w-11/12 shadow-lg'>
           </div>
         </Column>
-        <Column background='bg-secondary-200' header='Kreasi Kami' description='Karya kebanggaan dari KuenyaWZ' href='/location'>
+        <Column background='bg-secondary-200' header='Kalender' description='Perayaan Anda, Kreasi Kami' href='/kalender'>
           <div className='bg-white h-1/2 w-11/12 shadow-lg'>
           </div>
         </Column>
-        <Column background='bg-secondary-100' header='Kalender' description='Perayaan Anda, Kreasi Kami' href='/calendar'>
+        <Column background='bg-secondary-100' header='Tentang Kami' description='Ciptakan Pertemuan Manis' href='/tentang-kami'>
           <div className='bg-white h-1/2 w-11/12 shadow-lg'>
           </div>
         </Column>
@@ -29,19 +32,19 @@ type ColumnProps = {
   background: "bg-secondary-100" | "bg-secondary-200" | "bg-secondary-300";
   header: string;
   description: string;
-  href?: string;
+  href: string;
 };
 
 function Column(
   { children, background, header, description, href }: ColumnProps
 ) {
   return (
-    <a href={href} className={`${background}` + ' grow flex justify-center items-center px-[20%] py-[6%] hover:bg-secondary-400 transition ease-in-out duration-300'}>
+    <Link to={href} className={`${background}` + ' grow flex justify-center items-center px-[20%] py-[6%] hover:bg-secondary-400 transition ease-in-out duration-300'}>
       <div className='h-full w-full flex flex-col gap-4 items-center justify-center'>
         {children}
         <h2 className='font-fancy text-4xl text-center font-bold tracking-wide'>{header}</h2>
         <p className='font-semi text-xl text-center'>{description}</p>
       </div>
-    </a>
+    </Link>
   );
 }
