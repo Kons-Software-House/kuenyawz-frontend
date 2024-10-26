3a1cf73945b82901c135272e342fce3aefcc2640
trustacean <pizzacrustacean15@gmail.com>
2024-10-26 20:50:04 +0700

    chore: increased transform range

--- src/components/core/Container.tsx
+++ src/components/core/Container.tsx
@@ -6,7 +6,7 @@ type ContainerProps = {
 
 export default function Container({ children }: ContainerProps) {
   const { scrollYProgress } = useScroll();
-  const y = useTransform(scrollYProgress, [0, 1], [0, -20]);
+  const y = useTransform(scrollYProgress, [0, 1], [0, -30]);
   return (
     <motion.div className="flex justify-center mb-5 mt-5" style={{ y }} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.8, duration: 0.5, ease: 'easeInOut' }}>
       <div className="container flex flex-col items-center lg:w-9/12">
