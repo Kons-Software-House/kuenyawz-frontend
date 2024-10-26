e7629a014bc568c145525d7d61913dc368b6e71a
trustacean <pizzacrustacean15@gmail.com>
2024-10-26 16:53:59 +0700

    feat: added login modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { useEffect } from "react";
 import { AnimatePresence } from "framer-motion";
 import { Route, Routes, useLocation } from "react-router-dom";
+import { useModal } from "./contexts/ModalContext";
 import Navbar from "./components/core/Navbar";
+import LoginModal from "./components/modals/LoginModal"
 import LandingView from "./views/LandingView";
 import AboutUsView from "./views/AboutUsView";
 import CalendarView from "./views/CalendarView";
 
 function App() {
   const location = useLocation();
+  const { showLoginModal } = useModal();
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -16,6 +19,7 @@ function App() {
   return (
     <>
       <Navbar />
+      {showLoginModal && <LoginModal />}
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
           <Route path="/" element={<LandingView />} />
