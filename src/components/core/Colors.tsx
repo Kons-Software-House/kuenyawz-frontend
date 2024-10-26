0ac5889c9b666d7d249a79bd1c8907b90dc26848
trustacean <pizzacrustacean15@gmail.com>
2024-10-26 19:44:59 +0700

    feat: added lighter border colors

--- src/components/core/Colors.tsx
+++ src/components/core/Colors.tsx
@@ -11,3 +11,13 @@ export const LighterBackgroundColors = {
     'bg-tetriary-500': 'bg-tetriary-550',
     'bg-tetriary-600': 'bg-tetriary-650',
 }
+
+export const LighterBorderColors = {
+    'bg-secondary-200': 'border-secondary-250',
+    'bg-tetriary-100': 'border-tetriary-150',
+    'bg-tetriary-200': 'border-tetriary-250',
+    'bg-tetriary-300': 'border-tetriary-350',
+    'bg-tetriary-400': 'border-tetriary-450',
+    'bg-tetriary-500': 'border-tetriary-550',
+    'bg-tetriary-600': 'border-tetriary-650',
+}
