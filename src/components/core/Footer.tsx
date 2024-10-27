dfe7dc060f4f2ee9813509ad1b084f9aaeac3bed
trustacean <pizzacrustacean15@gmail.com>
2024-10-27 14:52:32 +0700

    feat: create Footer.tsx

---
+++ src/components/core/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-black text-white p-5">
+      <p>&copy; ©2024 KuenyaWZ. Licensed under the GNU General Public License v3.0.</p>
+    </footer>
+  )
+}
