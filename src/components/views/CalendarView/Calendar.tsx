bda2a89b29d96398b253e71d5572ffd550fb4912
trustacean <pizzacrustacean15@gmail.com>
2024-10-27 19:17:47 +0700

    chore: removed avaailability table from calendar

--- src/components/views/CalendarView/Calendar.tsx
+++ src/components/views/CalendarView/Calendar.tsx
@@ -1,81 +1,53 @@
 export default function Calendar() {
   return (
     <>
-      <div className="flex gap-x-4 justify-center w-full">
-        <div className={`aspect-[18/17] grid grid-cols-7 gap-x-2 bg-secondary-500 p-6 shadow-xl rounded-md w-2/3 max-w-[46rem]`} >
-          <YearMonth year={2021} month={8} />
-          <div className="grid grid-cols-7 col-span-7 gap-x-2 mb-3">
-            <WeekDay day="Sun" />
-            <WeekDay day="Mon" />
-            <WeekDay day="Tue" />
-            <WeekDay day="Wed" />
-            <WeekDay day="Thu" />
-            <WeekDay day="Fri" />
-            <WeekDay day="Sat" />
-          </div>
-          <Date date="30" thisMonth={false} eventType={0} />
-          <Date date="31" thisMonth={false} eventType={1} />
-          <Date date="1" thisMonth={true} eventType={1} />
-          <Date date="2" thisMonth={true} eventType={1} />
-          <Date date="3" thisMonth={true} eventType={0} />
-          <Date date="4" thisMonth={true} eventType={0} />
-          <Date date="5" thisMonth={true} eventType={0} />
-          <Date date="6" thisMonth={true} eventType={0} />
-          <Date date="7" thisMonth={true} eventType={0} />
-          <Date date="8" thisMonth={true} eventType={0} />
-          <Date date="9" thisMonth={true} eventType={2} />
-          <Date date="10" thisMonth={true} eventType={0} />
-          <Date date="11" thisMonth={true} eventType={0} />
-          <Date date="12" thisMonth={true} eventType={3} />
-          <Date date="13" thisMonth={true} eventType={0} />
-          <Date date="14" thisMonth={true} eventType={0} />
-          <Date date="15" thisMonth={true} eventType={0} />
-          <Date date="16" thisMonth={true} eventType={0} />
-          <Date date="17" thisMonth={true} eventType={0} />
-          <Date date="18" thisMonth={true} eventType={1} />
-          <Date date="19" thisMonth={true} eventType={1} />
-          <Date date="20" thisMonth={true} eventType={1} />
-          <Date date="21" thisMonth={true} eventType={1} />
-          <Date date="22" thisMonth={true} eventType={0} />
-          <Date date="23" thisMonth={true} eventType={0} />
-          <Date date="24" thisMonth={true} eventType={0} />
-          <Date date="25" thisMonth={true} eventType={4} />
-          <Date date="26" thisMonth={true} eventType={0} />
-          <Date date="27" thisMonth={true} eventType={0} />
-          <Date date="28" thisMonth={true} eventType={0} />
-          <Date date="29" thisMonth={true} eventType={0} />
-          <Date date="30" thisMonth={true} eventType={0} />
-          <Date date="1" thisMonth={false} eventType={0} />
-          <Date date="2" thisMonth={false} eventType={0} />
-          <Date date="3" thisMonth={false} eventType={2} />
-        </div >
-        <div className="bg-secondary-500 aspect-[1/2] py-12 px-8 flex flex-col gap-4 text-clear text-xl shadow-md rounded-xl h-full">
-          <div className="text-inherit flex items-center gap-2">
-            <div className="aspect-[1/1] h-8 bg-availability-200 border rounded-full"></div>
-            <p>
-              Dipesan
-            </p>
-          </div>
-          <div className="text-inherit flex items-center gap-2">
-            <div className="aspect-[1/1] h-8 bg-availability-300 border rounded-full"></div>
-            <p>
-              Libur
-            </p>
-          </div>
-          <div className="text-inherit flex items-center gap-2">
-            <div className="aspect-[1/1] h-8 bg-availability-400 border rounded-full"></div>
-            <p>
-              Erm
-            </p>
-          </div>
-          <div className="text-inherit flex items-center gap-2">
-            <div className="aspect-[1/1] h-8 bg-availability-500 border rounded-full"></div>
-            <p>
-              Erm
-            </p>
-          </div>
+      <div className={`aspect-[18/17] grid grid-cols-7 gap-x-2 bg-secondary-500 p-6 shadow-xl rounded-md w-2/3 max-w-[46rem]`} >
+        <YearMonth year={2021} month={8} />
+        <div className="grid grid-cols-7 col-span-7 gap-x-2 mb-3">
+          <WeekDay day="Sun" />
+          <WeekDay day="Mon" />
+          <WeekDay day="Tue" />
+          <WeekDay day="Wed" />
+          <WeekDay day="Thu" />
+          <WeekDay day="Fri" />
+          <WeekDay day="Sat" />
         </div>
-      </div>
+        <Date date="30" thisMonth={false} eventType={0} />
+        <Date date="31" thisMonth={false} eventType={1} />
+        <Date date="1" thisMonth={true} eventType={1} />
+        <Date date="2" thisMonth={true} eventType={1} />
+        <Date date="3" thisMonth={true} eventType={0} />
+        <Date date="4" thisMonth={true} eventType={0} />
+        <Date date="5" thisMonth={true} eventType={0} />
+        <Date date="6" thisMonth={true} eventType={0} />
+        <Date date="7" thisMonth={true} eventType={0} />
+        <Date date="8" thisMonth={true} eventType={0} />
+        <Date date="9" thisMonth={true} eventType={2} />
+        <Date date="10" thisMonth={true} eventType={0} />
+        <Date date="11" thisMonth={true} eventType={0} />
+        <Date date="12" thisMonth={true} eventType={3} />
+        <Date date="13" thisMonth={true} eventType={0} />
+        <Date date="14" thisMonth={true} eventType={0} />
+        <Date date="15" thisMonth={true} eventType={0} />
+        <Date date="16" thisMonth={true} eventType={0} />
+        <Date date="17" thisMonth={true} eventType={0} />
+        <Date date="18" thisMonth={true} eventType={1} />
+        <Date date="19" thisMonth={true} eventType={1} />
+        <Date date="20" thisMonth={true} eventType={1} />
+        <Date date="21" thisMonth={true} eventType={1} />
+        <Date date="22" thisMonth={true} eventType={0} />
+        <Date date="23" thisMonth={true} eventType={0} />
+        <Date date="24" thisMonth={true} eventType={0} />
+        <Date date="25" thisMonth={true} eventType={4} />
+        <Date date="26" thisMonth={true} eventType={0} />
+        <Date date="27" thisMonth={true} eventType={0} />
+        <Date date="28" thisMonth={true} eventType={0} />
+        <Date date="29" thisMonth={true} eventType={0} />
+        <Date date="30" thisMonth={true} eventType={0} />
+        <Date date="1" thisMonth={false} eventType={0} />
+        <Date date="2" thisMonth={false} eventType={0} />
+        <Date date="3" thisMonth={false} eventType={2} />
+      </div >
     </>
   )
 }
