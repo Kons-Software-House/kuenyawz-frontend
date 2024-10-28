29497a10e8f2cfe39e6d74161e7f41ce1fa5aae0
trustacean <pizzacrustacean15@gmail.com>
2024-10-29 00:14:51 +0700

    style: adjusted colors for better consistency

--- src/components/core/Colors.tsx
+++ src/components/core/Colors.tsx
@@ -1,5 +1,5 @@
 export type BackgroundColors = {
-    background?: 'bg-secondary-200' | 'bg-tetriary-100' | 'bg-tetriary-200' | 'bg-tetriary-300' | 'bg-tetriary-400' | 'bg-tetriary-500' | 'bg-tetriary-600';
+    background?: 'bg-secondary-200' | 'bg-tetriary-100' | 'bg-tetriary-200' | 'bg-tetriary-300' | 'bg-tetriary-400' | 'bg-tetriary-500';
 }
 
 export const LighterBackgroundColors = {
@@ -9,7 +9,6 @@ export const LighterBackgroundColors = {
     'bg-tetriary-300': 'bg-tetriary-350',
     'bg-tetriary-400': 'bg-tetriary-450',
     'bg-tetriary-500': 'bg-tetriary-550',
-    'bg-tetriary-600': 'bg-tetriary-650',
 }
 
 export const LighterBorderColors = {
@@ -19,5 +18,4 @@ export const LighterBorderColors = {
     'bg-tetriary-300': 'border-tetriary-350',
     'bg-tetriary-400': 'border-tetriary-450',
     'bg-tetriary-500': 'border-tetriary-550',
-    'bg-tetriary-600': 'border-tetriary-650',
 }
--- src/contexts/TransitionColorContext.tsx
+++ src/contexts/TransitionColorContext.tsx
@@ -1,8 +1,8 @@
 import { createContext, useContext, useState, ReactNode } from 'react';
 
 type TransitionColorContextType = {
-  transitionColor: 'bg-secondary-200' | 'bg-tetriary-100' | 'bg-tetriary-200' | 'bg-tetriary-300' | 'bg-tetriary-400' | 'bg-tetriary-500' | 'bg-tetriary-600';
-  setTransitionColor: (color: 'bg-secondary-200' | 'bg-tetriary-100' | 'bg-tetriary-200' | 'bg-tetriary-300' | 'bg-tetriary-400' | 'bg-tetriary-500' | 'bg-tetriary-600') => void;
+  transitionColor: 'bg-secondary-200' | 'bg-tetriary-100' | 'bg-tetriary-200' | 'bg-tetriary-300' | 'bg-tetriary-400' | 'bg-tetriary-500';
+  setTransitionColor: (color: 'bg-secondary-200' | 'bg-tetriary-100' | 'bg-tetriary-200' | 'bg-tetriary-300' | 'bg-tetriary-400' | 'bg-tetriary-500') => void;
 };
 
 const TransitionColorContext = createContext<TransitionColorContextType | undefined>(undefined);
@@ -12,7 +12,7 @@ type TransitionColorProviderProps = {
 };
 
 export function TransitionColorProvider({ children }: TransitionColorProviderProps) {
-  const [transitionColor, setTransitionColor] = useState<'bg-secondary-200' | 'bg-tetriary-100' | 'bg-tetriary-200' | 'bg-tetriary-300' | 'bg-tetriary-400' | 'bg-tetriary-500' | 'bg-tetriary-600'>('bg-secondary-200');
+  const [transitionColor, setTransitionColor] = useState<'bg-secondary-200' | 'bg-tetriary-100' | 'bg-tetriary-200' | 'bg-tetriary-300' | 'bg-tetriary-400' | 'bg-tetriary-500'>('bg-secondary-200');
 
   return (
     <TransitionColorContext.Provider value={{ transitionColor, setTransitionColor }}>
