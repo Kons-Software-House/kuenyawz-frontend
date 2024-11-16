310c28ef36cb0c05be9bbaeb14dd4471b1776cd4
trustacean <pizzacrustacean15@gmail.com>
2024-11-16 15:12:07 +0700

    chore: finishing up

--- src/views/admin/AddNewProductView.tsx
+++ src/views/admin/AddNewProductView.tsx
@@ -1,9 +1,48 @@
+import 'react-image-crop/dist/ReactCrop.css'
 import Sidebar from "../../components/admin/Sidebar";
-import { Formik, Form, Field } from "formik";
+import ImageCropperModal from '../../components/admin/modals/ImageCropperModal';
+
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useModal } from '../../contexts/ModalContext';
+import { useRef, useState } from 'react';
+
 
 export default function AddNewProductView() {
+  const { showImageCropperModal, setShowImageCropperModal } = useModal();
+  const sixByNineRef = useRef<string>('');
+  const oneByOneRef = useRef<string>('');
+  const fiveBySixRef = useRef<string>('');
+  const [ratio, setRatio] = useState<"6/9" | "1/1" | "5/6">("6/9");
+
+  const updatePreviewImages = (ratio: "6/9" | "1/1" | "5/6", imageUrl: string) => {
+    switch (ratio) {
+      case "6/9":
+        sixByNineRef.current = imageUrl;
+        break;
+      case "1/1":
+        oneByOneRef.current = imageUrl;
+        break;
+      case "5/6":
+        fiveBySixRef.current = imageUrl;
+        break;
+    }
+  }
+
+  const validateInput = (values: any) => {
+    const errors: any = {};
+    if (!sixByNineRef.current) errors.sixByNine = "Foto 6/9 tidak boleh kosong";
+    if (!oneByOneRef.current) errors.oneByOne = "Foto 1/1 tidak boleh kosong";
+    if (!fiveBySixRef.current) errors.fiveBySix = "Foto 5/6 tidak boleh kosong";
+    if (!values.name) errors.name = "Nama produk tidak boleh kosong";
+    if (!values.tagline) errors.tagline = "Tagline tidak boleh kosong";
+    if (!values.description) errors.description = "Deskripsi produk tidak boleh kosong";
+    if (!values.variants[0].type && !values.variants[1].type && !values.variants[2].type) errors.variants = "Setidaknya satu varian harus diisi";
+
+    return errors;
+  }
   return (
     <>
+      {showImageCropperModal && <ImageCropperModal aspectRatio={ratio} onClose={updatePreviewImages} />}
       <div className="flex">
         <Sidebar />
         <div className="w-full">
@@ -20,11 +59,41 @@ export default function AddNewProductView() {
                   { price: 0, type: '', minQuantity: 0, maxQuantity: 0 },
                   { price: 0, type: '', minQuantity: 0, maxQuantity: 0 },
                 ],
-              }} onSubmit={(values) => { console.log(values) }}>
+              }} onSubmit={(values) => { console.log(values) }} validate={validateInput}>
               <Form className="flex flex-col gap-4">
+                <h2>Foto Produk</h2>
+                <div className="flex flex-col grid grid-cols-1 md:grid-cols-3 gap-10 w-full mb-4">
+                  <div className="p-2 flex flex-col gap-1">
+                    <label htmlFor="photo1">Foto 1 (6/9)</label>
+                    <div className='flex justify-center aspect-[6/9] border-4 border-tetriary-400 lg:mx-14'>
+                      <img src={sixByNineRef.current} className='text-center' alt='Pilih Foto' />
+                    </div>
+                    <div className='grow' />
+                    <button type="button" className='bg-tetriary-550 rounded-lg' onClick={() => { setRatio("6/9"); setShowImageCropperModal(true) }}>Pilih Foto</button>
+                  </div>
+                  <div className="p-2 flex flex-col gap-1">
+                    <label htmlFor="photo2">Foto 2 (1/1)</label>
+                    <div className='flex justify-center aspect-[1/1] border-4 border-tetriary-400'>
+                      <img src={oneByOneRef.current} className='text-center' alt='Pilih Foto' />
+                    </div>
+                    <div className='grow' />
+                    <button type="button" className='bg-tetriary-550 rounded-lg' onClick={() => { setRatio("1/1"); setShowImageCropperModal(true) }}>Pilih Foto</button>
+                  </div>
+                  <div className="p-2 flex flex-col gap-1">
+                    <label htmlFor="photo3">Foto 3 (5/6)</label>
+                    <div className='flex justify-center aspect-[5/6] border-4 border-tetriary-400'>
+                      <img src={fiveBySixRef.current} className='text-center' alt='Pilih Foto' />
+                    </div>
+                    <div className='grow' />
+                    <button type="button" className='bg-tetriary-550 rounded-lg' onClick={() => { setRatio("5/6"); setShowImageCropperModal(true) }}>Pilih Foto</button>
+                  </div>
+                </div>
                 <Field name="name" type="text" placeholder="Nama Produk" className="p-2 border border-gray-300 rounded" />
+                <ErrorMessage name="name" component="div" className="text-red-500" />
                 <Field name="tagline" type="text" placeholder="Tagline" className="p-2 border border-gray-300 rounded" />
+                <ErrorMessage name="tagline" component="div" className="text-red-500" />
                 <Field name="description" as="textarea" placeholder="Deskripsi Produk" className="p-2 border border-gray-300 rounded" />
+                <ErrorMessage name="description" component="div" className="text-red-500" />
                 <Field name="category" as="select" className="p-2 border border-gray-300 rounded">
                   <option value="cake">Cake</option>
                   <option value="pastry">Pastry</option>
@@ -37,6 +106,7 @@ export default function AddNewProductView() {
                   <VariantField name="variants[1]" variant="Varian 2" />
                   <VariantField name="variants[2]" variant="Varian 3" />
                 </div>
+                <ErrorMessage name="variants" component="div" className="text-red-500" />
                 <button type="submit" className="p-2 px-4 bg-secondary-300 border-2 border-secondary-100 rounded-lg">Buat Produk Baru</button>
               </Form>
             </Formik>
