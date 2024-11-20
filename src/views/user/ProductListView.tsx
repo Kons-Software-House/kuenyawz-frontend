d7189a6089fa9b0e4d0752fdef44fd8990603016
trustacean <pizzacrustacean15@gmail.com>
2024-11-21 04:30:26 +0700

    that merge was fked up

--- src/views/user/ProductListView.tsx
+++ src/views/user/ProductListView.tsx
@@ -1,7 +1,10 @@
 import { motion } from "framer-motion"
 import { Link } from "react-router-dom";
 import { useTransitionColor } from "../../contexts/TransitionColorContext";
-import { LighterBorderColors } from "../../components/user/core/Colors";
+import { CategoryColors, LighterBorderColors } from "../../components/user/core/Colors";
+import { useEffect, useState } from "react";
+import { Product } from "../../types/Product";
+import { retrieveProducts } from "../../services/ProducApiService";
 import UpperSection from "../../components/user/core/UpperSection"
 import Container from "../../components/user/core/Container"
 
@@ -136,13 +139,12 @@ export default function ProductListView() {
 }
 
 type CardProps = {
-  background: 'bg-tetriary-100' | 'bg-tetriary-200' | 'bg-tetriary-300' | 'bg-tetriary-400' | 'bg-tetriary-500';
-  picture?: string
-  title?: string
+  product: Product
 }
 
-function Card({ background, picture, title }: CardProps) {
+function Card({ product }: CardProps) {
   const { setTransitionColor } = useTransitionColor();
+  const background = CategoryColors[product.category] as "bg-tetriary-100" | "bg-tetriary-200" | "bg-tetriary-300" | "bg-tetriary-400" | "bg-tetriary-500";
   const beforeBackground = {
     'bg-tetriary-100': 'before:bg-tetriary-100',
     'bg-tetriary-200': 'before:bg-tetriary-200',
@@ -155,13 +157,13 @@ function Card({ background, picture, title }: CardProps) {
     hover: { x: "0%", opacity: 1 },
   }
   return (
-    <Link to="/produk" onClick={() => { setTransitionColor(background) }}>
+    <Link to={`/produk/${product.productId}`} onClick={() => { setTransitionColor(background) }}>
       <motion.div className={`aspect-[2/3] relative flex justify-center items-center ${LighterBorderColors[background]} border-4
        before:absolute before:-inset-0 before:rotate-6 ${beforeBackground[background]} hover:before:rotate-12 before:-z-10 before:transition-transform before:duration-300`} initial='default' whileHover='hover'>
         <motion.p className="absolute font-fancy text-2xl lg:text-3xl text-white text-shadow-sm overflow-hidden text-center" variants={hoverVariant}>
-          {title}
+          {product.name}
         </motion.p>
-        <img src={picture} alt="" className="w-full h-full object-cover" />
+        <img src={(product.images ? product.images[0] : '')} alt="" className="w-full h-full object-cover" />
       </motion.div>
     </Link>
   )
