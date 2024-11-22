24d116036aca0d72f303f81adafdfc271cf0f28f
trustacean <pizzacrustacean15@gmail.com>
2024-11-22 19:01:29 +0700

    feat: add register account API service

--- src/services/AuthApiService.tsx
+++ src/services/AuthApiService.tsx
@@ -5,3 +5,8 @@ const apiClient = axios.create({
   withCredentials: true
 });
 
+export const registerAccount = async (fullName: string, phone: string, password: string): Promise<any> => {
+  const response = await apiClient.post('/auth/register', { fullName, phone, password });
+  return response.data;
+}
+
