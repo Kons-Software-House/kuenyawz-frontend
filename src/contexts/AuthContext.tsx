58acdd4b788e894336b0b7c85bce41fa0eca2c89
trustacean <pizzacrustacean15@gmail.com>
2024-11-23 20:55:22 +0700

    feat: add send and verify otp handler

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,11 +1,16 @@
 import { createContext, useContext, useEffect, useRef, useState } from "react";
-import { registerAccount, loginAccount, checkAuthentication, refreshAuthentication, logoutAccount } from "../services/AuthApiService";
+import { loginAccount, checkAuthentication, refreshAuthentication, logoutAccount, registerAccount, requestOtp, verifyOtp } from "../services/AuthApiService";
 
 type AuthContextType = {
   isAuthenticated: boolean;
-  handleLogin: (phone: string, password: string) => boolean;
-  handleRegister: (fullName: string, phone: string, password: string) => boolean;
+  otpCountdown: number;
+  fullName: string;
+  phone: string;
+  handleLogin: (phone: string, password: string) => Promise<boolean>;
   handleLogout: () => void;
+  handleRegister: (fullName: string, phone: string, password: string) => Promise<boolean>;
+  handleSendOtp: (phone: string) => void;
+  handleVerifyOtp: (otp: string) => Promise<boolean>;
 };
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
@@ -20,6 +25,9 @@ export const useAuth = () => {
 
 export default function AuthProvider({ children }: { children: React.ReactNode }) {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [otpCountdown, setOtpCountdown] = useState(0);
+  const [fullName, setFullName] = useState('');
+  const [phone, setPhone] = useState('');
   const isInitialMount = useRef(true);
 
   useEffect(() => {
@@ -36,9 +44,22 @@ export default function AuthProvider({ children }: { children: React.ReactNode }
     return () => clearInterval(interval);
   }, []);
 
+  useEffect(() => {
+    if (otpCountdown > 0) {
+      const interval = setInterval(() => {
+        setOtpCountdown(otpCountdown - 1);
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+  }, [otpCountdown]);
+
   const checkAuth = async () => {
     try {
-      await checkAuthentication();
+      const response = await checkAuthentication();
+      setIsAuthenticated(true);
+      setFullName(response.fullName);
+      setPhone(response.phone);
     } catch (error: any) {
       setIsAuthenticated(false);
     }
@@ -49,12 +70,11 @@ export default function AuthProvider({ children }: { children: React.ReactNode }
       await refreshAuthentication();
       setIsAuthenticated(true);
     } catch (error: any) {
-      console.log(error);
       setIsAuthenticated(false);
     }
   }
 
-  const handleLogin = (phone: string, password: string) => {
+  const handleLogin = async (phone: string, password: string) => {
     try {
       loginAccount(phone, password);
       setIsAuthenticated(true);
@@ -66,28 +86,55 @@ export default function AuthProvider({ children }: { children: React.ReactNode }
     }
   }
 
-  const handleRegister = (fullName: string, phone: string, password: string) => {
+  const handleLogout = () => {
+    try {
+      logoutAccount();
+      setIsAuthenticated(false);
+    } catch (error: any) {
+      alert("Failed to logout: " + error.response.data.message);
+      console.log(error);
+    }
+  }
+
+  const handleRegister = async (fullName: string, phone: string, password: string) => {
     try {
       registerAccount(fullName, phone, password);
       setIsAuthenticated(true);
       return true;
     } catch (error: any) {
       alert("Failed to register account: " + error.response.data.message);
+      setIsAuthenticated(false);
       return false;
     }
   }
 
-  const handleLogout = () => {
+  const handleSendOtp = async (phone: string) => {
     try {
-      logoutAccount();
-      setIsAuthenticated(false);
+      await requestOtp(phone);
+      if (phone.length < 8) {
+        return;
+      }
+      setOtpCountdown(60);
+      setPhone(phone);
     } catch (error: any) {
-      console.log(error);
+      alert("Failed to send OTP: " + error.response.data.message);
+      console.error(error);
+    }
+  }
+
+  const handleVerifyOtp = async (otp: string) => {
+    try {
+      await verifyOtp(phone, otp);
+      return true;
+    } catch (error: any) {
+      alert("Failed to verify OTP: " + error.response.data.message);
+      console.error(error);
+      return false;
     }
   }
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, handleLogin, handleRegister, handleLogout }}>
+    <AuthContext.Provider value={{ isAuthenticated, otpCountdown, fullName, phone, handleLogin, handleLogout, handleRegister, handleSendOtp, handleVerifyOtp }}>
       {children}
     </AuthContext.Provider>
   );
