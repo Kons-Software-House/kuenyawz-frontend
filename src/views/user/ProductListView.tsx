917c2e93b36776682b6594068d867fd45f610a16
trustacean <pizzacrustacean15@gmail.com>
2024-11-23 22:44:48 +0700

    chore: update Product List View to use ProductCard component

--- src/views/user/ProductListView.tsx
+++ src/views/user/ProductListView.tsx
@@ -1,12 +1,9 @@
-import { motion } from "framer-motion"
-import { Link } from "react-router-dom";
-import { useTransitionColor } from "../../contexts/TransitionColorContext";
-import { CategoryColors, LighterBorderColors } from "../../components/user/core/Colors";
 import { useEffect, useState } from "react";
 import { Product } from "../../types/Product";
 import { retrieveProducts } from "../../services/ProducApiService";
 import UpperSection from "../../components/user/core/UpperSection"
 import Container from "../../components/user/core/Container"
+import ProductCard from "../../components/user/core/ProductCard";
 
 export default function ProductListView() {
   const [isLoading, setIsLoading] = useState(true)
@@ -106,7 +103,7 @@ export default function ProductListView() {
         {isLoading && <p>Memuat...</p>}
         <div className='grid grid-cols-3 lg:grid-cols-4 p-8 w-full gap-8 lg:gap-14'>
           {products.map((product) => (
-            <Card key={product.productId} product={product} />
+            <ProductCard key={product.productId} product={product} />
           ))}
 
         </div >
@@ -135,36 +132,5 @@ export default function ProductListView() {
         </div>
       </Container>
     </>
-  )
-}
-
-type CardProps = {
-  product: Product
-}
-
-function Card({ product }: CardProps) {
-  const { setTransitionColor } = useTransitionColor();
-  const background = CategoryColors[product.category] as "bg-tetriary-100" | "bg-tetriary-200" | "bg-tetriary-300" | "bg-tetriary-400" | "bg-tetriary-500";
-  const beforeBackground = {
-    'bg-tetriary-100': 'before:bg-tetriary-100',
-    'bg-tetriary-200': 'before:bg-tetriary-200',
-    'bg-tetriary-300': 'before:bg-tetriary-300',
-    'bg-tetriary-400': 'before:bg-tetriary-400',
-    'bg-tetriary-500': 'before:bg-tetriary-500',
-  }
-  const hoverVariant = {
-    default: { x: "20%", opacity: 0 },
-    hover: { x: "0%", opacity: 1 },
-  }
-  return (
-    <Link to={`/produk/${product.productId}`} onClick={() => { setTransitionColor(background) }}>
-      <motion.div className={`aspect-[2/3] relative flex justify-center items-center ${LighterBorderColors[background]} border-4
-       before:absolute before:-inset-0 before:rotate-6 ${beforeBackground[background]} hover:before:rotate-12 before:-z-10 before:transition-transform before:duration-300`} initial='default' whileHover='hover'>
-        <motion.p className="absolute font-fancy text-2xl lg:text-3xl text-white text-shadow-sm overflow-hidden text-center" variants={hoverVariant}>
-          {product.name}
-        </motion.p>
-        <img src={(product.images ? product.images[0] : '')} alt="" className="w-full h-full object-cover" />
-      </motion.div>
-    </Link>
   )
 }
