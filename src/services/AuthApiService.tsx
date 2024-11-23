8fe67450c6ab4f2e558936217c1c554f3abe77ea
trustacean <pizzacrustacean15@gmail.com>
2024-11-23 13:21:12 +0700

    feat: add request otp and verivy otp API service

--- src/services/AuthApiService.tsx
+++ src/services/AuthApiService.tsx
@@ -5,11 +5,6 @@ const apiClient = axios.create({
   withCredentials: true
 });
 
-export const registerAccount = async (fullName: string, phone: string, password: string): Promise<any> => {
-  const response = await apiClient.post('/auth/register', { fullName, phone, password });
-  return response.data;
-}
-
 export const loginAccount = async (phone: string, password: string): Promise<any> => {
   const response = await apiClient.post('/auth/login', { phone, password });
   return response.data;
@@ -27,5 +22,20 @@ export const refreshAuthentication = async (): Promise<any> => {
 
 export const logoutAccount = async (): Promise<any> => {
   const response = await apiClient.post('/auth/revoke');
+  return response.data;
+}
+
+export const registerAccount = async (fullName: string, phone: string, password: string): Promise<any> => {
+  const response = await apiClient.post('/auth/register', { fullName, phone, password });
+  return response.data;
+}
+
+export const requestOtp = async (phone: string): Promise<any> => {
+  const response = await apiClient.post('/auth/otp/request', { phone });
+  return response.data;
+}
+
+export const verifyOtp = async (phone: string, otp: string): Promise<any> => {
+  const response = await apiClient.post('/auth/otp/verify', { phone, otp });
   return response.data;
 }
