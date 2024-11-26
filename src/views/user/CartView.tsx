59aad00125f0df222554506b52ec48fdbf9805e8
Edward Vito <129828532+Trustacean@users.noreply.github.com>
2024-11-26 15:38:23 +0700

    chore: removed unnecessary  components

--- src/views/user/CartView.tsx
+++ src/views/user/CartView.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react"
-import Container from "../../components/user/core/Container"
-import UpperSection from "../../components/user/core/UpperSection"
-import { useAuth } from "../../contexts/AuthContext"
 import { retrieveUserCart, deleteFromUserCart, updateCartItem } from "../../services/UserApiService"
 import { CartItem } from "../../types/CartItem"
 import { Link } from "react-router-dom"
 import { Field, Form, Formik } from "formik"
+import { formatToIdr } from "../../types/Formatter"
+import Container from "../../components/user/core/Container"
+import UpperSection from "../../components/user/core/UpperSection"
 
 export default function CartView() {
   const [cartItems, setCartItems] = useState<CartItem[]>([])
@@ -58,7 +58,7 @@ export default function CartView() {
     <>
       <UpperSection title="Keranjang" subtitle="Daftar Kreasi Pilihan Anda" />
       <Container>
-        <div className="grid grid-cols-1 2xl:grid-cols-3 w-full text-sm 2xl:gap-4">
+        <div className="grid grid-cols-1 w-full font-clear">
           <div className="col-span-2">
             <div className="grid grid-cols-1 bg-secondary-500 w-full p-8 rounded-md shadow-lg gap-2">
               <div className="flex pl-10 pr-2 p-2 font-bold bg-secondary-100 rounded-md shadow-md gap-2">
@@ -73,7 +73,7 @@ export default function CartView() {
               ))}
             </div>
           </div>
-          <div className="mt-4 2xl:mt-0 flex flex-col gap-4">
+          <div className="mt-4 flex flex-col gap-4">
             <CartSummary cartItems={cartItems} />
             <button className="bg-secondary-500 rounded-md shadow-lg p-2">
               <Link to="/checkout" className="text-black text-lg font-semi">Lanjutkan ke Pembayaran</Link>
@@ -122,7 +122,7 @@ function CartItemComponent({ cartItem, handleDeleteCartItem, handleUpdateCartIte
             </div>
           </div>
         </div>
-        <span className="w-28 text-center">Rp {variant?.price}</span>
+        <span className="w-28 text-center">{formatToIdr(variant?.price ?? 0)}</span>
         <Formik initialValues={{ quantity: cartItem.quantity }} onSubmit={(values) => { handleUpdateCartItem(cartItem.cartItemId.toString(), values.quantity) }}>
           {({ submitForm, setFieldValue }) => (
             <Form className="w-14 flex justify-center">
@@ -145,7 +145,7 @@ function CartItemComponent({ cartItem, handleDeleteCartItem, handleUpdateCartIte
             </Form>
           )}
         </Formik>
-        <span className="w-28 text-center">Rp {(variant?.price ?? 0) * cartItem.quantity}</span>
+        <span className="w-28 text-center">{formatToIdr((variant?.price ?? 0) * cartItem.quantity)}</span>
         <button className="w-6 text-red-900 font-bold font-clear text-sm" onClick={() => handleDeleteCartItem(cartItem.cartItemId.toString())}>[X]</button>
       </div>
     </>
@@ -163,62 +163,17 @@ function CartSummary({ cartItems }: CartSummaryProps) {
     )
     return acc + (variant?.price ?? 0) * cartItem.quantity
   }, 0)
-  const total = subtotal + 3000 + 20000
-  const formattedSubtotal = subtotal.toLocaleString('id-ID')
-  const formattedTotal = total.toLocaleString('id-ID')
+
+
+  const formattedSubtotal = formatToIdr(subtotal)
 
   return (
     <div className="bg-secondary-500 w-full p-8 rounded-md shadow-lg">
-      <span className="text-xl font-semibold">Rincian Belanja</span>
+      <span className="text-xl font-semibold">Rincian</span>
       <hr className="bg-black my-2 border-black" />
-      <span className="text-lg font-semibold">Total Belanja</span>
       <div className="grid grid-cols-2">
         <span>Subtotal</span>
-        <span className="text-end">Rp {formattedSubtotal}</span>
-      </div>
-      <hr className="bg-black my-2 border-black" />
-      <span className="text-lg font-semibold">Biaya Transaksi</span>
-      <div className="grid grid-cols-2">
-        <span>Biaya Layanan</span>
-        <span className="text-end">Rp 3.000</span>
-        <span>Biaya Pengiriman</span>
-        <span className="text-end">Rp 20.000</span>
-      </div>
-      <hr className="bg-black my-2 border-black" />
-      <span className="text-lg font-semibold">Total Pembayaran</span>
-      <div className="grid grid-cols-2">
-        <span>Total</span>
-        <span className="text-end">Rp {formattedTotal}</span>
-      </div>
-    </div>
-  )
-}
-
-function BuyingDetails() {
-  const { phone } = useAuth()
-  return (
-    <div className="bg-secondary-500 w-full p-8 rounded-md shadow-lg">
-      <span className="text-xl font-semibold">Detail Pembelian</span>
-      <hr className="bg-black my-2 border-black" />
-      <div className="grid gap-4">
-        <div className="grid grid-cols-2">
-          <span className="block">Nomor Telepon</span>
-          <span className="block text-end">
-            +62 {phone.slice(0, 3)} {phone.slice(3, 7)} {phone.slice(7, 14)}
-          </span>
-        </div>
-        <div>
-          <label htmlFor="date" className="block">Tanggal Pengiriman</label>
-          <input type="date" id="date" className="w-full p-2 rounded-md" />
-        </div>
-        <div>
-          <label htmlFor="address" className="block">Alamat</label>
-          <textarea id="address" className="w-full p-2 rounded-md"></textarea>
-        </div>
-        <div>
-          <label htmlFor="note" className="block">Catatan</label>
-          <textarea id="note" className="w-full p-2 rounded-md"></textarea>
-        </div>
+        <span className="text-end">{formattedSubtotal}</span>
       </div>
     </div>
   )
