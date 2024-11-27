9a6a567feb09b7fcac667479ae14ed1e2a50d011
Edward Vito <129828532+Trustacean@users.noreply.github.com>
2024-11-27 23:15:28 +0700

    chore: add temporary closeddate inputter

--- src/views/admin/AdminCalendarView.tsx
+++ src/views/admin/AdminCalendarView.tsx
@@ -1,10 +1,30 @@
+import { Formik, Field, Form } from "formik"
+
+import { createClosedDates } from "../../services/CalendarApiService"
 import Sidebar from "../../components/admin/views/AdminDashboardView/Sidebar"
+import Calendar from "../../components/user/views/CalendarView/Calendar"
 
 export default function AdminCalendarView() {
+  const handleSubmit = async (values: any) => {
+    try {
+      await createClosedDates(values.date, "RESERVED");
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
     <div className="flex">
       <Sidebar />
-      AdminCalendarView
-    </div>
+      <div className="bg-red-200 w-full">
+        <Formik initialValues={{ date: '' }} onSubmit={(values) => { handleSubmit(values) }}>
+          <Form>
+            <Calendar />
+            <Field type="date" name="date" />
+            <button type="submit">Submit</button>
+          </Form>
+        </Formik>
+      </div >
+    </div >
   )
 }
