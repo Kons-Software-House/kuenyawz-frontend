c5a674397e2071d2892e91d4702884ad0ced5c9f
Edward Vito <129828532+Trustacean@users.noreply.github.com>
2024-11-27 17:48:39 +0700

    feat: create AdminCalendarView.tsx

---
+++ src/views/admin/AdminCalendarView.tsx
@@ -0,0 +1,10 @@
+import Sidebar from "../../components/admin/views/AdminDashboardView/Sidebar"
+
+export default function AdminCalendarView() {
+  return (
+    <div className="flex">
+      <Sidebar />
+      AdminCalendarView
+    </div>
+  )
+}
