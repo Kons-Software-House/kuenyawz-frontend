ee70a5330df0c214d866835d75c5415a9216ab4c
Edward Vito <129828532+Trustacean@users.noreply.github.com>
2024-11-28 21:20:41 +0700

    style: add backdrop blur

--- src/components/user/core/Backdrop.tsx
+++ src/components/user/core/Backdrop.tsx
@@ -13,7 +13,8 @@ export default function Backdrop({ children, onClose, width = 'w-10/12 sm:w-7/12
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className="fixed h-full w-full bg-black/40 flex items-center justify-center z-50">
+      transition={{ duration: 0.3 }}
+      className="fixed h-full w-full bg-black/40 flex items-center justify-center z-50 backdrop-blur-sm">
       <div className={`bg-slate-100 shadow-lg rounded-lg ${width}`} onClick={(e) => { e.stopPropagation() }}>
         {children}
       </div>
