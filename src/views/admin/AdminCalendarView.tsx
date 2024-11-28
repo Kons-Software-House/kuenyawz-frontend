9ac92cee6e273505bb652fbacb011a9a52d09b87
Edward Vito <129828532+Trustacean@users.noreply.github.com>
2024-11-28 21:11:28 +0700

    feat: working closed dates management

--- src/views/admin/AdminCalendarView.tsx
+++ src/views/admin/AdminCalendarView.tsx
@@ -1,13 +1,24 @@
 import { Formik, Field, Form } from "formik"
 
-import { createClosedDates } from "../../services/CalendarApiService"
+import { createClosedDates, deleteClosedDates } from "../../services/CalendarApiService"
 import Sidebar from "../../components/admin/views/AdminDashboardView/Sidebar"
 import Calendar from "../../components/user/views/CalendarView/Calendar"
+import AvailabilityTable from "../../components/user/views/CalendarView/AvailabilityTable"
 
 export default function AdminCalendarView() {
-  const handleSubmit = async (values: any) => {
+  const handleCreateClosedDates = async (values: any) => {
     try {
-      await createClosedDates(values.date, "RESERVED");
+      await createClosedDates(values.date, "CLOSED");
+      window.location.reload();
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  const handleDeleteClosedDates = async (values: any) => {
+    try {
+      await deleteClosedDates(values.date);
+      window.location.reload();
     } catch (e) {
       console.error(e);
     }
@@ -16,14 +27,29 @@ export default function AdminCalendarView() {
   return (
     <div className="flex">
       <Sidebar />
-      <div className="bg-red-200 w-full">
-        <Formik initialValues={{ date: '' }} onSubmit={(values) => { handleSubmit(values) }}>
-          <Form>
+      <div className="w-full">
+        <div className="m-4">
+          <div className="flex gap-2 mt-6 grow lg:flex-row flex-col">
             <Calendar />
-            <Field type="date" name="date" />
-            <button type="submit">Submit</button>
-          </Form>
-        </Formik>
+            <AvailabilityTable />
+          </div>
+          <div className="bg-secondary-500 mt-6 p-6 max-w-[59rem]">
+            <Formik initialValues={{ date: '' }} onSubmit={(values) => { handleCreateClosedDates(values) }}>
+              <Form className="grid grid-cols-3 w-[30rem] mb-2">
+                <label htmlFor="date" className="text-clear text-md mr-2">Tambah Hari Libur</label>
+                <Field type="date" name="date" className="rounded-md p-2 mr-2" />
+                <button type="submit" className="bg-secondary-100 rounded p-2 w-28">Tambahkan</button>
+              </Form>
+            </Formik>
+            <Formik initialValues={{ date: '' }} onSubmit={(values) => { handleDeleteClosedDates(values) }}>
+              <Form className="grid grid-cols-3 w-[30rem]">
+                <label htmlFor="date" className="text-clear text-md mr-4">Hapus Hari Libur</label>
+                <Field type="date" name="date" className="rounded-md p-2 mr-2" />
+                <button type="submit" className="bg-secondary-100 rounded p-2 w-28">Hapus</button>
+              </Form>
+            </Formik>
+          </div>
+        </div>
       </div >
     </div >
   )
