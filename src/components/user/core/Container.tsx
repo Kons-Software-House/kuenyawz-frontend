16e1d0762b91da8bf1efc685b1112cf8e18fc2e7
Edward Vito <129828532+Trustacean@users.noreply.github.com>
2024-11-30 21:32:52 +0700

    style: update Container component layout and spacing

--- src/components/user/core/Container.tsx
+++ src/components/user/core/Container.tsx
@@ -8,8 +8,8 @@ export default function Container({ children }: ContainerProps) {
   const { scrollYProgress } = useScroll();
   const y = useTransform(scrollYProgress, [0, 1], [0, -30]);
   return (
-    <motion.div className="flex justify-center mb-5 mt-5" style={{ y }} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.8, duration: 0.5, ease: 'easeInOut' }}>
-      <div className="container flex flex-col items-center lg:w-9/12">
+    <motion.div className="flex justify-center mt-2 lg:mt-5 min-h-screen" style={{ y }} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.8, duration: 0.5, ease: 'easeInOut' }}>
+      <div className="container flex flex-col items-center px-4 lg:px-0 lg:w-9/12">
         {children}
       </div>
     </motion.div>
