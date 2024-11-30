16dfd771d2542b10dd222478186df5456af75716
Edward Vito <129828532+Trustacean@users.noreply.github.com>
2024-12-01 01:00:05 +0700

    style: adjust padding and font sizes in AvailabilityTable for improved layout and readability

--- src/components/user/views/CalendarView/AvailabilityTable.tsx
+++ src/components/user/views/CalendarView/AvailabilityTable.tsx
@@ -1,6 +1,6 @@
 export default function AvailabilityTable() {
   return (
-    <div className="bg-secondary-500 py-12 pl-8 pr-16 flex flex-col gap-4 text-clear text-md xl:text-xl shadow-md rounded-xl">
+    <div className="bg-secondary-500 py-4 lg:py-12 px-6 lg:pl-8 lg:pr-16 flex flex-col gap-4 text-clear text-sm lg:text-xl shadow-md rounded-xl">
       <div className="text-inherit flex items-center gap-2">
         <div className="aspect-[1/1] h-6 xl:h-8 bg-availability-400 border rounded-full"></div>
         <p>
