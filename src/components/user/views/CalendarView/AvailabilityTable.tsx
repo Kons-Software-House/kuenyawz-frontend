a59d622e261afbd325c22835f54938e2a4130062
Edward Vito <129828532+Trustacean@users.noreply.github.com>
2024-12-01 14:48:25 +0700

    style: add informational message to AvailabilityTable for improved user guidance

--- src/components/user/views/CalendarView/AvailabilityTable.tsx
+++ src/components/user/views/CalendarView/AvailabilityTable.tsx
@@ -1,6 +1,6 @@
 export default function AvailabilityTable() {
   return (
-    <div className="bg-secondary-500 py-8 lg:py-12 px-8 lg:pl-8 lg:pr-16 flex flex-col gap-4 text-clear text-sm lg:text-xl shadow-md rounded-xl">
+    <div className="bg-secondary-500 py-8 lg:py-12 px-8 flex flex-col gap-4 text-clear text-sm lg:text-xl shadow-md rounded-xl">
       <div className="text-inherit flex items-center gap-2">
         <div className="aspect-[1/1] h-6 xl:h-8 bg-availability-400 border rounded-full"></div>
         <p>
@@ -11,6 +11,11 @@ export default function AvailabilityTable() {
         <div className="aspect-[1/1] h-6 xl:h-8 bg-availability-300 border rounded-full"></div>
         <p>
           Libur
+        </p>
+      </div>
+      <div className="h-full flex items-end text-sm underline underline-offset-2 font-clear">
+        <p>
+          Tiap Pesanan memerlukan waktu minimal 2 hari sebelum hari-H
         </p>
       </div>
     </div>
