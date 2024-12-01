dd072f9bc469dc56e53643f8f3e18888f4d7146c
Edward Vito <129828532+Trustacean@users.noreply.github.com>
2024-12-01 15:04:53 +0700

    style: refactor formatToIdr function and add formatDateString utility for improved date formatting

--- src/types/Formatter.tsx
+++ src/types/Formatter.tsx
@@ -1,3 +1,10 @@
-export function formatToIdr(num: number) {
+export const formatToIdr = (num: number) => {
     return 'Rp ' + num.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
 }
+
+export const formatDateString = (date: Date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+};
