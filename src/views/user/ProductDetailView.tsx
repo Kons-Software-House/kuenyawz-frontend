3b17a8fe6632a8d9f014f36ff196f2cfc1029cd9
Edward Vito <129828532+Trustacean@users.noreply.github.com>
2024-12-01 13:43:32 +0700

    style: adjust responsive layout and spacing in ProductDetailView for improved usability

--- src/views/user/ProductDetailView.tsx
+++ src/views/user/ProductDetailView.tsx
@@ -61,42 +61,42 @@ export default function ProductDetailPage() {
           {showAddToCartModal && <AddToCartModal variants={product.variants} />}
           <UpperSection title={product.name} background={CategoryColors[product.category] as 'bg-secondary-200' | 'bg-tetriary-100' | 'bg-tetriary-200' | 'bg-tetriary-300' | 'bg-tetriary-400' | 'bg-tetriary-500'} />
           <Container>
-            <div className="grid grid-cols-6 lg:grid-cols-11 gap-2 lg:gap-10 w-full mb-4 lg:-mt-20">
+            <div className="grid grid-cols-6 sm:grid-cols-11 gap-2 sm:gap-10 w-full mb-4 md:-mt-8 lg:-mt-20">
               <Column span2={false} moveRange={40}>
-                <p className="font-semi text-xl md:text-2xl lg:text-3xl mb-4 object-cover font-semibold order-last lg:order-first">
+                <p className="font-semi text-xl md:text-2xl sm:text-3xl mb-4 object-cover font-semibold order-last sm:order-first">
                   {product.tagline}
                 </p>
-                <div className={`overflow-hidden aspect-[3/4.5] bg-gray-100 w-full rounded-lg border-4 lg:border-8 ${LighterBorderColors[CategoryColors[product.category as keyof typeof CategoryColors] as keyof typeof LighterBorderColors]} rounded-lg`}>
+                <div className={`overflow-hidden aspect-[3/4.5] bg-gray-100 w-full rounded-lg border-4 sm:border-8 ${LighterBorderColors[CategoryColors[product.category as keyof typeof CategoryColors] as keyof typeof LighterBorderColors]} rounded-lg`}>
                   <img className="aspect-[3/4.5] w-full object-cover rounded-lg shadow-md scale-105 hover:scale-110 transition ease-in-out duration-300" draggable="false" alt="Product" src={product.images[0]} />
                 </div>
-                <div className="lg:h-12 lg:w-full">
+                <div className="sm:h-12 sm:w-full">
                 </div>
               </Column>
               <Column span2={false} moveRange={0}>
-                <p className="font-poppins sm:text-mds md:text-md lg:text-lg xl:text-xl mb-4 text-justify tracking-tight pb-2 order-last lg:order-first">
+                <p className="font-poppins sm:text-mds md:text-md sm:text-lg xl:text-xl mb-4 text-justify tracking-tight pb-2 order-last sm:order-first">
                   {product.description}
                 </p>
-                <div className="h-12 w-full order-last lg:hidden">
+                <div className="h-12 w-full order-last sm:hidden">
                 </div>
-                <div className={`overflow-hidden aspect-[1/1] bg-gray-100 w-full rounded-lg border-4 lg:border-8 ${LighterBorderColors[CategoryColors[product.category as keyof typeof CategoryColors] as keyof typeof LighterBorderColors]} rounded-lg`}>
+                <div className={`overflow-hidden aspect-[1/1] bg-gray-100 w-full rounded-lg border-4 sm:border-8 ${LighterBorderColors[CategoryColors[product.category as keyof typeof CategoryColors] as keyof typeof LighterBorderColors]} rounded-lg`}>
                   <img className="aspect-[1/1] w-full object-cover rounded-lg shadow-md scale-105 hover:scale-110 transition ease-in-out duration-300" draggable="false" alt="Product" src={product.images[1]} />
                 </div>
               </Column>
               <Column span2={true} moveRange={20}>
-                <div className={`overflow-hidden aspect-[10/12] bg-gray-100 w-full rounded-lg border-4 lg:border-8  ${LighterBorderColors[CategoryColors[product.category as keyof typeof CategoryColors] as keyof typeof LighterBorderColors]} rounded-lg`}>
+                <div className={`overflow-hidden aspect-[10/12] bg-gray-100 w-full rounded-lg border-4 sm:border-8  ${LighterBorderColors[CategoryColors[product.category as keyof typeof CategoryColors] as keyof typeof LighterBorderColors]} rounded-lg`}>
                   <img className="aspect-[10/12] w-full scale-105 hover:scale-110 transition ease-in-out duration-300" draggable="false" alt="Product" src={product.images[2]} />
                 </div>
-                <div className="hidden lg:block">
+                <div className="hidden sm:block">
                   <AddToCartButton color={CategoryColors[product.category] as 'bg-tetriary-100' | 'bg-tetriary-200' | 'bg-tetriary-300' | 'bg-tetriary-400' | 'bg-tetriary-500'} onClick={() => setShowAddToCartModal(true)} />
                 </div>
               </Column>
             </div>
-            <div className="block lg:hidden w-full">
+            <div className="block sm:hidden w-full">
               <AddToCartButton color={CategoryColors[product.category] as 'bg-tetriary-100' | 'bg-tetriary-200' | 'bg-tetriary-300' | 'bg-tetriary-400' | 'bg-tetriary-500'} onClick={() => setShowAddToCartModal(true)} />
             </div>
-            <h4 className="w-full text-center text-xl lg:text-3xl font-semi mt-4">Cocok ditambah dengan</h4>
+            <h4 className="w-full text-center text-xl sm:text-3xl font-semi mt-4">Cocok ditambah dengan</h4>
             <div className="w-full flex justify-center">
-              <div className="flex mt-6 w-full lg:p-8 lg:gap-20 justify-between mb-4">
+              <div className="flex mt-6 w-full sm:p-8 sm:gap-20 justify-between mb-4">
                 {isRecommendedLoading ? <div>Loading</div> : recommendedProducts.map((product, index) => (
                   <div key={`product-${productId}-${product.productId.toString()}-${index}`} className="bg-gray-100 aspect-[1/1] w-1/4">
                     <ProductCard product={product} />
@@ -105,7 +105,7 @@ export default function ProductDetailPage() {
               </div>
             </div>
           </Container>
-          <div className={`hidden lg:block absolute left-0 right-0 -mt-[46rem] xl:-mt-[50rem] md:h-[6rem] -z-10 ${LighterBackgroundColors[CategoryColors[product.category as keyof typeof CategoryColors] as keyof typeof LighterBackgroundColors]}`} />
+          <div className={`hidden sm:block absolute left-0 right-0 -mt-[46rem] xl:-mt-[50rem] md:h-[6rem] -z-10 ${LighterBackgroundColors[CategoryColors[product.category as keyof typeof CategoryColors] as keyof typeof LighterBackgroundColors]}`} />
         </>
       }
     </>
@@ -123,7 +123,7 @@ function Column({ span2, children, moveRange }: ColumnProps) {
   const y = useTransform(scrollYProgress, [0, 1], [0, -moveRange]);
 
   return (
-    <motion.div className={"" + (span2 ? "col-span-6 lg:col-span-5 order-first lg:order-last " : "col-span-3 ")} style={{ y }}>
+    <motion.div className={"" + (span2 ? "col-span-6 sm:col-span-5 order-first sm:order-last " : "col-span-3 ")} style={{ y }}>
       <div className="flex items-end justify-end flex h-full w-full">
         <div className="flex flex-col gap-2 w-full">
           {children}
