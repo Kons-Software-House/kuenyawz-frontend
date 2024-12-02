8518e3d47f5f6fed2eaab1b1ff19c6621518f944
Edward Vito <129828532+Trustacean@users.noreply.github.com>
2024-12-02 17:23:48 +0700

    feat: implement AdminOrderListView component for displaying and paginating orders

---
+++ src/views/admin/AdminOrderListView.tsx
@@ -0,0 +1,84 @@
+import Sidebar from "../../components/admin/views/AdminDashboardView/Sidebar";
+import { useEffect, useState } from "react";
+import { retrieveOrders } from "../../services/OrderApiService";
+import { Order } from "../../types/Orders";
+
+
+export default function AdminOrderListView() {
+  const [loading, setLoading] = useState(true)
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const fetchOrders = async (newPage?: number) => {
+    setLoading(true);
+    if (newPage && newPage < 0) {
+      newPage = 0;
+    }
+    if (newPage && newPage > totalPages - 1) {
+      newPage = undefined;
+    }
+
+    try {
+      const params = {
+        page: newPage,
+        pageSize: 5
+      };
+
+      const response = await retrieveOrders(params);
+      setOrders(response.content);
+      console.log(page);
+      setPage(response.page.number);
+      setTotalPages(response.page.totalPages);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchOrders();
+  }, [])
+
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="w-full p-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-2xl font-bold">Orders</h1>
+        </div>
+        {loading ? <p>Loading...</p> : <>
+          <div className="mt-4 text-sm">
+            <table className="w-full bg-secondary-250">
+              <thead>
+                <tr className="border-b bg-secondary-100">
+                  <th className="text-left p-2">Alamat Pengiriman</th>
+                  <th className="text-left p-2">Tanggal</th>
+                  <th className="text-center p-2">Status</th>
+                  <th className="text-center p-2">Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                {orders.map(order => (
+                  <tr key={order.purchaseId} className="border-b border-gray-200 odd:bg-[#ffffe8] even:bg-[#fefed0]">
+                    <td className="p-2">{order.fullAddress}</td>
+                    <td className="p-2">{order.eventDate}</td>
+                    <td className="text-center p-2">{order.status}</td>
+                    <td className="flex justify-center p-2">
+                      <button className="border-2 border-secondary-100 text-black px-4 py-1 rounded-md p-2">Detail</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="mt-4 flex justify-between items-center">
+            <button className="bg-secondary-100 text-black px-4 py-2 rounded-md" onClick={() => fetchOrders(page - 1)} disabled={page === 0}>Previous</button>
+            <button className="bg-secondary-100 text-black px-4 py-2 rounded-md" onClick={() => fetchOrders(page + 1)} disabled={page === totalPages - 1}>Next</button>
+          </div>
+        </>}
+      </div>
+    </div>
+  )
+}
