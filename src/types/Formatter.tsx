5f3055f876028cbb0614c0f211a400568cc16ac0
Edward Vito <129828532+Trustacean@users.noreply.github.com>
2024-12-06 14:37:45 +0700

    feat: update currency format prefix from 'Rp' to 'IDR' in formatToIdr function

--- src/types/Formatter.tsx
+++ src/types/Formatter.tsx
@@ -1,5 +1,5 @@
 export const formatToIdr = (num: number) => {
-    return 'Rp ' + num.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+    return 'IDR ' + num.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
 }
 
 export const formatDateString = (date: Date) => {
