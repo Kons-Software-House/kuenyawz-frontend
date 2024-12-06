bf318e69b870975a06b3b02620f9b40cce4c9e6f
Edward Vito <129828532+Trustacean@users.noreply.github.com>
2024-12-06 13:19:45 +0700

    feat: add localized order status translations for better user experience

---
+++ src/types/OrderStatus.tsx
@@ -0,0 +1,8 @@
+export const LocalizedOrderStatus = {
+    "PENDING": "Menunggu Pembayaran",
+    "CONFIRMING": "Menunggu Konfirmasi",
+    "CONFIRMED": "Dikonfirmasi",
+    "PROCESSING": "Sedang Diproses",
+    "DELIVERED": "Sudah Dikirim",
+    "CANCELLED": "Dibatalkan",
+}
