6d2919a9b099864f035ddd333126889f177d3cd1
Edward Vito <129828532+Trustacean@users.noreply.github.com>
2024-12-08 17:06:53 +0700

    fix: update footer text to shorten the developer name for clarity

--- src/components/user/core/Footer.tsx
+++ src/components/user/core/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-black text-white p-5 flex flex-col gap-2">
       <p className="w-full text-center">&copy; 2024 KuenyaWZ. Licensed under the GNU General Public License v3.0.</p>
-      <p className="w-full text-center">Designed and developed <a href="https://github.com/Kons-Software-House" className="underline">Kons Software House</a></p>
+      <p className="w-full text-center">Designed and developed <a href="https://github.com/Kons-Software-House" className="underline">Kons SW</a></p>
     </footer>
   )
 }
