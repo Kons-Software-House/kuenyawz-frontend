81e2a8a9e8d41db7e9504aad33ef4f24dac71599
Edward Vito <129828532+Trustacean@users.noreply.github.com>
2024-12-08 13:17:11 +0700

    feat: add OrdersGrowthChart to AdminDashboardView for visualizing order activity

--- src/views/admin/AdminDashboardView.tsx
+++ src/views/admin/AdminDashboardView.tsx
@@ -1,4 +1,5 @@
 import CategorySpreadChart from "../../components/admin/views/AdminDashboardView/CategorySpreadChart"
+import OrdersGrowthChart from "../../components/admin/views/OrdersGrowthChart"
 import OrderCount from "../../components/admin/views/AdminDashboardView/OrderCount"
 import Sidebar from "../../components/admin/views/AdminDashboardView/Sidebar"
 
@@ -20,6 +21,10 @@ export default function AdminDashboardView() {
             </div>
             <div className="shadow-lg border-2 border-secondary-100 rounded-lg">
             </div>
+          </div>
+          <div className="h-80 mt-8">
+            <h2 className="font-semibold font-clear">Grafik Aktivitas Pesanan</h2>
+            <OrdersGrowthChart />
           </div>
         </div>
       </div>
