c91ea732fe2afab7086374713c7402bba2e0d0a3
Edward Vito <129828532+Trustacean@users.noreply.github.com>
2024-12-08 14:40:57 +0700

    refactor: remove unused import of Link in ActiveOrderView

--- src/views/user/ActiveOrderView.tsx
+++ src/views/user/ActiveOrderView.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Link, useNavigate, useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 
 import { Order } from "../../types/Order"
 import { formatToIdr } from "../../types/Formatter"
