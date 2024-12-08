f3a3f1f613aabb07d17cf16d772a025274854616
Edward Vito <129828532+Trustacean@users.noreply.github.com>
2024-12-08 12:04:37 +0700

    feat: enhance AdminDashboardView layout with improved styling and structure

--- src/views/admin/AdminDashboardView.tsx
+++ src/views/admin/AdminDashboardView.tsx
@@ -2,15 +2,23 @@ import { CategorySpreadChart } from "../../components/admin/views/AdminDashboard
 import Sidebar from "../../components/admin/views/AdminDashboardView/Sidebar"
 
 export default function AdminDashboardView() {
+
   return (
     <>
       <div className="flex">
         <Sidebar />
-        <div className="w-full">
+        <div className="w-full p-4">
           <h1 className="font-semi text-2xl text-center p-4">Admin Dashboard</h1>
-          <div className="bg-red-200 h-80">
+          <div className="h-72 grid grid-cols-3 gap-4">
+            <div className="shadow-lg border-2 border-secondary-100 rounded-lg flex flex-col items-center">
+              <h2 className="font-semibold font-clear">Sebaran Kategori</h2>
+              <CategorySpreadChart />
+            </div>
+            <div className="shadow-lg border-2 border-secondary-100 rounded-lg">
+            </div>
+            <div className="shadow-lg border-2 border-secondary-100 rounded-lg">
+            </div>
           </div>
-          <CategorySpreadChart />
         </div>
       </div>
     </>
