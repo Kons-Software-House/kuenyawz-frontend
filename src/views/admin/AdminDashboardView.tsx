e79303d667f44d8fc5bcecd1b50f68ea1d63e2ce
Edward Vito <129828532+Trustacean@users.noreply.github.com>
2024-12-08 12:42:33 +0700

    feat: update import for CategorySpreadChart to default export

--- src/views/admin/AdminDashboardView.tsx
+++ src/views/admin/AdminDashboardView.tsx
@@ -1,4 +1,4 @@
-import { CategorySpreadChart } from "../../components/admin/views/AdminDashboardView/Charts"
+import CategorySpreadChart from "../../components/admin/views/AdminDashboardView/CategorySpreadChart"
 import OrderCount from "../../components/admin/views/AdminDashboardView/OrderCount"
 import Sidebar from "../../components/admin/views/AdminDashboardView/Sidebar"
 
@@ -10,7 +10,7 @@ export default function AdminDashboardView() {
         <Sidebar />
         <div className="w-full p-4">
           <h1 className="font-semi text-2xl text-center p-4">Admin Dashboard</h1>
-          <div className="h-72 grid grid-cols-3 gap-4">
+          <div className="grid grid-cols-3 gap-4">
             <div className="shadow-lg border-2 border-secondary-100 rounded-lg flex flex-col items-center">
               <h2 className="font-semibold font-clear">Sebaran Kategori</h2>
               <CategorySpreadChart />
