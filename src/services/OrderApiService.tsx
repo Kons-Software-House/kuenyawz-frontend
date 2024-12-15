2122dbf539e32b83aa0bd630339c141e607e805b
Edward Vito <129828532+Trustacean@users.noreply.github.com>
2024-12-16 02:56:18 +0700

    fix: add JSONbig for handling large integers in API responses

--- src/services/OrderApiService.tsx
+++ src/services/OrderApiService.tsx
@@ -1,7 +1,11 @@
 import axios from "axios";
+import JSONbig from 'json-bigint';
 
 const apiClient = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
+    transformResponse: [(data) => {
+        return JSONbig.parse(data);
+    }],
     withCredentials: true,
     headers: {
         "ngrok-skip-browser-warning": true
