eb77204fa62ec9d15fc90b45f2587693061e3c5b
Edward Vito <129828532+Trustacean@users.noreply.github.com>
2024-12-17 21:52:31 +0700

    feat: fetch and display product image in ProductCard component

--- src/components/user/core/ProductCard.tsx
+++ src/components/user/core/ProductCard.tsx
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 import { CategoryColors, LighterBorderColors } from "./Colors";
 import { Product } from "../../../types/Product";
 import { useTransitionColor } from "../../../contexts/TransitionColorContext";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { retrieveProductImage } from "../../../services/ImageApiService";
 
 type ProductCardProps = {
   product: Product
@@ -12,6 +13,7 @@ type ProductCardProps = {
 
 export const ProductCard = React.memo(({ product }: ProductCardProps) => {
   const { setTransitionColor } = useTransitionColor();
+  const [imageURI, setImageURI] = useState<string>('');
   const background = CategoryColors[product.category] as "bg-tetriary-100" | "bg-tetriary-200" | "bg-tetriary-300" | "bg-tetriary-400" | "bg-tetriary-500";
   const beforeBackground = {
     'bg-tetriary-100': 'before:bg-tetriary-100',
@@ -24,6 +26,15 @@ export const ProductCard = React.memo(({ product }: ProductCardProps) => {
     default: { x: "20%", opacity: 0 },
     hover: { x: "0%", opacity: 1 },
   }
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      const response = await retrieveProductImage(product.images[0]);
+      setImageURI(response);
+    }
+    fetchImage();
+  }, [product.images])
+
   return (
     <Link to={`/product/${product.productId}`} onClick={() => { setTransitionColor(background) }}>
       <motion.div className={`relative aspect-[2/3] relative flex justify-center items-center ${LighterBorderColors[background]} border-4
@@ -31,7 +42,7 @@ export const ProductCard = React.memo(({ product }: ProductCardProps) => {
         <motion.p className="absolute font-fancy text-lg sm:text-2xl xl:text-3xl text-white text-shadow-sm overflow-hidden text-center" variants={hoverVariant}>
           {product.name}
         </motion.p>
-        <img src={(product.images ? product.images[0] : '')} alt="" className="w-full h-full object-cover" />
+        <img src={(imageURI)} alt="" className="w-full h-full object-cover" />
         <div className={`absolute bottom-0 left-0 right-0 text-[0.4rem] sm:text-xs text-center block lg:hidden bg-white`}>
           {product.name}
         </div>
